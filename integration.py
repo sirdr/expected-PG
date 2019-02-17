7efb55c00ca3ac01125fd2f6655a808ece9c4c02
James Smith <jamessmith@DN51v44u.SUNet>
2019-02-17 14:33:03 -0800

    integration start

--- integration.py
+++ integration.py
@@ -1,5 +1,17 @@
-def f(state, action):
-    return np.sum(action)
+import scipy.integrate as integrate
+import numpy as np
 
-def compute_integral_over_action_space(f):
-    return whatever value
+
+def f(x0,x1,x2,x3):
+    return x0+x1+x2+x3
+
+
+def compute_integral_over_action_space(f, limits, tol=None):
+    result = integrate.nquad(f,limits)
+    return result[0]
+
+#action= np.array([1,2])
+limits = [[-5,1], [-9,1], [0,10], [-3,3]]
+
+result = compute_integral_over_action_space(f,limits)
+print(result)
