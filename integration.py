e6713d57d73bda61a5f2c31944e50bc7008a406c
James Smith <jamessmith@DN51v44u.SUNet>
2019-02-17 14:44:35 -0800

    updated function argument

--- integration.py
+++ integration.py
@@ -2,16 +2,24 @@ import scipy.integrate as integrate
 import numpy as np
 
 
-def f(x0,x1,x2,x3):
-    return x0+x1+x2+x3
+def f(*args):
+    return np.sum(args)
+
 
 
 def compute_integral_over_action_space(f, limits, tol=None):
     result = integrate.nquad(f,limits)
     return result[0]
 
-#action= np.array([1,2])
+action= np.array([1,2, 3, 4])
 limits = [[-5,1], [-9,1], [0,10], [-3,3]]
 
 result = compute_integral_over_action_space(f,limits)
 print(result)
+
+
+#def f(*args):
+#    print(args[0][1])
+#    return np.sum(args)
+
+#print(f([0,1,2]))
