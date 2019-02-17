4ef73ccab065d5d2bdbfede70c78d75005bd6330
James Smith <jamessmith@Jamess-MacBook-Pro-3.local>
2019-02-17 15:27:29 -0800

    updated low high

--- integration.py
+++ integration.py
@@ -6,20 +6,16 @@ def f(*args):
     return np.sum(args)
 
 
-
-def compute_integral_over_action_space(f, limits, tol=None):
+def compute_integral_over_action_space(f, low, high, tol=None):
+    limits = []
+    for i in range(0,len(low)):
+        lim = [low[i], high[i]]
+        limits.append(lim)
     result = integrate.nquad(f,limits)
     return result[0]
 
-action= np.array([1,2, 3, 4])
-limits = [[-5,1], [-9,1], [0,10], [-3,3]]
 
-result = compute_integral_over_action_space(f,limits)
-print(result)
-
-
-#def f(*args):
-#    print(args[0][1])
-#    return np.sum(args)
-
-#print(f([0,1,2]))
+#low = [-5, -9, 0 ,-3]
+#high = [1, 1, 10, 3]
+#result = compute_integral_over_action_space(f,low, high)
+#print(result)
