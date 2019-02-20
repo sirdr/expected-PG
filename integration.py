b4c0da331d2868993af0ca76e9fb354e31d307e6
James Smith <jamessmith@DN51v44u.SUNet>
2019-02-19 22:12:30 -0800

    Added Adaptive Quad function using Simpson's rule

--- integration.py
+++ integration.py
@@ -1,6 +1,77 @@
+from __future__ import division # python 2 compat
 import scipy.integrate as integrate
 import numpy as np
+import time
 
+
+
+def compute_integral(f, low, high, shape, prec):
+    integral = np.zeros(shape)
+    a_range = np.linspace(low, high, (high-low)/prec)
+    for a in a_range:
+        integral += f(a)*prec
+    return integral
+
+
+###Adaptive Quadrature###
+def compute_integral_asr(f, low, high, tol):
+    """Uses Adaptive Simpson's Method"""
+    return quad_asr(f, low, high, tol)
+
+
+def _quad_simpsons_mem(f, a, fa, b, fb):
+    """Evaluates the Simpson's Rule, also returning m and f(m) to reuse"""
+    m = (a+b) / 2
+    fm = f(m)
+    return (m, fm, abs(b-a) / 6 * (fa + 4 * fm + fb))
+
+def _quad_asr(f, a, fa, b, fb, eps, whole, m, fm):
+    """
+    Efficient recursive implementation of adaptive Simpson's rule.
+    Function values at the start, middle, end of the intervals are retained.
+    """
+    lm, flm, left  = _quad_simpsons_mem(f, a, fa, m, fm)
+    rm, frm, right = _quad_simpsons_mem(f, m, fm, b, fb)
+    delta = left + right - whole
+    if np.linalg.norm(delta, ord=np.inf) <= 15*eps: #abs(delta) <= 15 * eps:
+        return left + right + delta / 15
+    return _quad_asr(f, a, fa, m, fm, eps/2, left , lm, flm) +\
+           _quad_asr(f, m, fm, b, fb, eps/2, right, rm, frm)
+
+
+def quad_asr(f, a, b, eps):
+    """Integrate f from a to b using Adaptive Simpson's Rule with max error of eps."""
+    fa, fb = f(a), f(b)
+    m, fm, whole = _quad_simpsons_mem(f, a, fa, b, fb)
+    return _quad_asr(f, a, fa, b, fb, eps, whole, m, fm)
+
+##Tests###
+
+#def f(a):
+#    mat = np.zeros((3,3))
+#    for i in range(0,3):
+#        mat[0][i] = np.exp(3*a)*np.sin(a+1)
+#    for i in range(0,3):
+#        mat[1][i] = np.cos(a+2)
+#    for i in range(0,3):
+#        mat[2][i]= np.sin(a+3)
+#    return mat
+
+#start = time.time()
+#result = compute_integral(f, -1, 3, (3,3), .0001)
+#end = time.time()
+#print(result)
+#print(end-start)
+
+#start = time.time()
+#result= compute_integral_asr(f, -1, 3, 1e-03)
+#end = time.time()
+#print(result)
+#print(end-start)
+
+
+
+####Old###
 
 #def f(*args):
 #    v = np.zeros(3)
@@ -25,23 +96,4 @@ import numpy as np
 #print(result)
 
 
-#def f(a):
-#    mat = np.zeros((3,3))
-#    for i in range(0,3):
-#        mat[0][i] = a+1
-#    for i in range(0,3):
-#        mat[1][i] = a+2
-#    for i in range(0,3):
-#        mat[2][i]= a+3
-#    return mat
 
-
-def compute_integral(f, low, high, shape, prec):
-    integral = np.zeros(shape)
-    a_range = np.linspace(low, high, (high-low)/prec)
-    for a in a_range:
-        integral += f(a)*prec
-    return integral
-
-#result = compute_integral(f, 0, 2, (3,3), .001)
-#print(result)
