27007eff4f0e068f0c38cc677666b1396c9241c5
Loren AC <loren.k.ac@gmail.com>
2019-05-16 22:35:15 -0700

    comments in utils

--- utils.py
+++ utils.py
@@ -48,12 +48,12 @@ def get_policy(policy_type, env, config, writer, num_actions):
 def get_writer_name(policy_type, config, seed, use_target, env_name, num_actions, run_id='NA', exp_id='NA', evaluation=-1, expected_sarsa=False):
     name = "{}-{}-{}-{}-exp_id={}-run_id={}-seed={}-num_episodes={}".format(policy_type, env_name, config.critic_lr, config.policy_lr, exp_id, run_id, seed, config.num_episodes)
     
-    name = name + '-policy_lr_decay={}-policy_lr_step={}'.format(config.policy_lr_decay, config.policy_lr_step_size)
+    #name = name + '-policy_lr_decay={}-policy_lr_step={}'.format(config.policy_lr_decay, config.policy_lr_step_size)
 
     if policy_type == 'reinforce':
         pass
     else:
-        name = name + '-critic_lr_decay={}-critic_lr_step={}'.format(config.critic_lr_decay, config.critic_lr_step_size)
+        #name = name + '-critic_lr_decay={}-critic_lr_step={}'.format(config.critic_lr_decay, config.critic_lr_step_size)
         if policy_type == 'mc':
             name = name+'-num_samples={}'.format(num_actions)
         elif policy_type == 'integrate':
